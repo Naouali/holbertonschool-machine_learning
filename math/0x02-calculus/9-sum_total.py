9154390f8abb102b9f66fe77f9b7a49216b87ab9
Naouali <elnaoualinebil@gmail.com>
2020-12-09 20:32:51 +0100

    Push

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -9,6 +9,6 @@ def summation_i_squared(n):
     n: stopping condition
     return: total
     """
-    if type(n) != int:
+    if type(n) != int or n < 1:
         return None
     return int(n / 6 + (n ** 2) / 2 + pow(n, 3) / 3)
