9240119b6aa773be8f0c1c12639ddd996e678ea1
Naoualli <elnaoualinebil@gmail.com>
2021-01-21 20:39:57 +0000

    round

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -12,8 +12,8 @@ class Binomial:
         if data is None:
             if n <= 0:
                 raise ValueError("n must be a positive value")
-            if p < 0 or p > 1:
-                raise ValueError("p must e greater than 0 and less than 1")
+            if p <= 0 or p >= 1:
+                raise ValueError("p must be greater than 0 and less than 1")
             self.n = int(n)
             self.p = float(p)
         else:
@@ -21,6 +21,11 @@ class Binomial:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.p = (sum(data) / len(data)) / len(data)
-            self.n = int(round(len(data)))
-            self.p = (sum(data)/len(data) / self.n) * 2
+            mean = sum(data) / len(data)
+            variance = 0
+            for i in data:
+                variance += (i - mean) ** 2
+            variance = variance / len(data)
+            self.p = -1 * (variance / mean - 1)
+            self.n = round(mean / self.p)
+            self.p = mean / self.n
