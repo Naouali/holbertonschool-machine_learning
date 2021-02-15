fa475ed7f6874964d6d1658d161b12255f2656c7
Naouali <elnaoualinebil@gmail.com>
2021-02-15 15:59:55 +0100

    tst

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -11,7 +11,14 @@ def one_hot_encode(Y, classes):
     """
     encode
     """
-
+    if Y is None or classes is None:
+        return None
+    if len(Y) == 0:
+        return None
+    if classes < 0:
+        return None
+    if Y.max() >= classes:
+        return None
     hot = np.zeros((Y.shape[0], classes))
     for i in Y:
         j = np.where(Y == i)
