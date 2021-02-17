10fb07ee4a0a41fb678d54e7fa6960c685314d68
Naouali <elnaoualinebil@gmail.com>
2021-02-17 20:23:05 +0000

    test

---
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Keras sequatial
+"""
+
+
+import tensorflow as tf
+import tensorflow.keras as keras
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    build model
+    """
+    model = keras.Sequential()
+    for i in range(1, len(layers)):
+        model.add(keras.layers.Dropout(1 - keep_prob))
+        model.add(keras.layers.Dense(
+            layers[i],
+            activations[i],
+            input_shape=nx,
+            kernel_regularizer=keras.regularizers.l2(lambtha),
+        ))
+
+    return model
+
+
