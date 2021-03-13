8660fc8cd9afbb3470a6e33094c4829607117d63
Naouali <elnaoualinebil@gmail.com>
2021-03-13 17:37:53 +0100

    fix

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -13,7 +13,7 @@ class Yolo():
     """
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        self.model = K.models.load_model(model_path)
+        self.model = Keras.models.load_model(model_path)
         with open(classes_path, "r") as f:
             r = f.read().split("\n")[:-1]
         self.class_names = r
