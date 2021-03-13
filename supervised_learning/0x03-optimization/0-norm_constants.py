40e221ff000fc1575dc1e572015abd775edbdcc4
Naouali <elnaoualinebil@gmail.com>
2021-03-13 15:19:17 +0100

    norm

--- supervised_learning/0x03-optimization/0-norm_constants.py
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -1,32 +1,16 @@
 #!/usr/bin/env python3
 """
-<<<<<<< HEAD
-Norm
+Normalize
 """
 
-
-=======
-normal
-"""
-
->>>>>>> 982c764c6b1edb6530c5d08930d5fd2f637ec485
 import numpy as np
 
 
 def normalization_constants(X):
-<<<<<<< HEAD
-	"""
-	normalizing
-	"""
-	m = X.shape[0]
-	mean = np.sum(X/ m, axis=0)
-	std = np.sqrt(np.sum(((X - mean) ** 2), axis=0) / m)
-	return mean, std
-=======
     """
-    Normalize a matrix
+    normalizing
     """
-    mean = np.sum(X / X.shape[0], axis=0)
-    std = np.sqrt(np.sum(((X - mean) ** 2), axis=0) / X.shape[0])
+    m = X.shape[0]
+    mean = np.sum(X / m, axis=0)
+    std = np.sqrt(np.sum(((X - mean) ** 2), axis=0) / m)
     return mean, std
->>>>>>> 982c764c6b1edb6530c5d08930d5fd2f637ec485
