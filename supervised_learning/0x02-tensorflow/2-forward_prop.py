01d2aac1bd8066f026fdcce51cac9f42dfbbab7c
Naouali <elnaoualinebil@gmail.com>
2021-05-08 17:36:36 +0200

    forwrd

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -9,6 +9,9 @@ create_layer = __import__('1-create_layer').create_layer
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
+    """
+    forward propagation function for neural network
+    """
     for nodes, acts in zip(layer_sizes, activations):
         y = create_layer(x, nodes, acts)
         x = y
