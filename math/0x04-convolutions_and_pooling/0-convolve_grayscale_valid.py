e0e2a63bc93eea075408b171173ac16a3979baea
Naouali <elnaoualinebil@gmail.com>
2021-08-27 11:45:17 +0200

    pooling

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,21 +1,5 @@
 #!/usr/bin/env python3
+
 """
-perform a convolution
+Performa a convolution on a gray scale image
 """
-import numpy as np
-
-
-def convolve_grayscale_valid(images, kernel):
-    """
-    perform a convolution
-    """
-
-    m, h, w = images.shape
-    kh, kw = kernel.shape
-    cvns = np.zeros((m, int(h - kh + 1), int(w - kw + 1)))
-
-    for i in range(h - kh + 1):
-        for j in range(w - kw + 1):
-            cvns[:, i, j] = np.sum(
-                images[:, i: i + kh, j: j + kw] * kernel, axis=(1, 2))
-    return cvns
--- math/0x04-convolutions_and_pooling/README.md
+++ math/0x04-convolutions_and_pooling/README.md
@@ -1,1 +1,5 @@
-How convolutional neural network works
+
+
+*** Convolution  is a mathematical operation performed in neural network, that recognize image inside of the traditional matrix multiplication.
+
+*** convolution is passed throught a pooling layer, that reduce the dimension of the image and extract only the important features.
