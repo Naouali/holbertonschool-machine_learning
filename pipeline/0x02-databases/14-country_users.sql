20806d0c5aca6e3e79ec15f42f5c7dde5a4e651d
Naouali <elnaoualinebil@gmail.com>
2022-01-29 17:28:48 +0100

    test ENUM

---
+++ pipeline/0x02-databases/14-country_users.sql
@@ -0,0 +1,9 @@
+-- CREATE a table of unique users
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US',
+	PRIMARY KEY (id)
+);
