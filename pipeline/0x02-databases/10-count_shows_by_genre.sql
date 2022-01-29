f1eea69b29407cf28c5f1944fdafd8d77433e1ab
Naouali <elnaoualinebil@gmail.com>
2022-01-29 15:43:38 +0100

    count

---
+++ pipeline/0x02-databases/10-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- script to list all genres that have shows
+
+SELECT g.name AS genre, count(i.show_id) AS number_of_shows
+FROM tv_genres AS g
+INNER JOIN tv_show_genres as i
+ON g.id = i.genre_id
+GROUP BY g.name
+ORDER BY number_of_shows DESC;
