c13c686e2b1206e7c3d11f05269b3837ad0770d6
Naouali <elnaoualinebil@gmail.com>
2022-01-29 15:44:47 +0100

    Uppercase

--- pipeline/0x02-databases/10-count_shows_by_genre.sql
+++ pipeline/0x02-databases/10-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 
 SELECT g.name AS genre, count(i.show_id) AS number_of_shows
 FROM tv_genres AS g
-INNER JOIN tv_show_genres as i
+INNER JOIN tv_show_genres AS i
 ON g.id = i.genre_id
 GROUP BY g.name
 ORDER BY number_of_shows DESC;
