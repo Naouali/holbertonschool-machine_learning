47d67697109db8fab7cf726f46ba1bc91fd5e784
Naouali <elnaoualinebil@gmail.com>
2022-10-15 15:41:48 +0200

    shape of matrix

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -9,11 +9,8 @@ def matrix_shape(matrix):
     Input : 2D array
     Output: 1D Array shape of matrix
     """
-
     shape = []
-
     while type(matrix) is not int:
         shape.append(len(matrix))
         matrix = matrix[1]
-
     return shape
