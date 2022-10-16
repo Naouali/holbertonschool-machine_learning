3fe05bd39696cdae0dc763b026914b5d387e665b
Naouali <elnaoualinebil@gmail.com>
2022-10-16 14:29:26 +0200

    add

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,30 +4,15 @@ add two matrices element wise
 """
 
 
-def matrix_shape(matrix):
-    """
-    Input : 2D array
-    Output: 1D Array shape of matrix
-    """
-    shape = []
-    while type(matrix) is not int:
-        shape.append(len(matrix))
-        matrix = matrix[1]
-    return shape
-
-
 def add_arrays(arr1, arr2):
     """
     Input: Two 1D arrays
     Output: 1D array
     """
-    if len(arr1) != len(arr2):
-        return None
-    else:
-        added = []
-        for i, j in zip(arr1, arr2):
-            added.append(i + j)
-        return added
+    added = []
+    for i, j in zip(arr1, arr2):
+        added.append(i + j)
+    return added
 
 
 def add_matrices2D(mat1, mat2):
@@ -35,7 +20,7 @@ def add_matrices2D(mat1, mat2):
     add two matrices element wise
     """
     added = []
-    if matrix_shape(mat1) != matrix_shape(mat2):
+    if len(mat1[0]) != len(mat2[0]):
         return None
     else:
         for x, j in zip(mat1, mat2):
