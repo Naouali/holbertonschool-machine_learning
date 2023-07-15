411f2fd350a6dad8a3fbb0aaeaa13fb037c439f5
Naouali <elnaoualinebil@gmail.com>
2023-07-15 19:40:44 +0200

    Poisson class

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Python program to generate the poisson distribution
+    
+"""
+    
+    
+class Poisson:
+    def __init__(self, data=None, lambtha=1.):
+            
+        self.data = data
+        self.lambtha = float(lambtha)
+        if data is None:
+            self.data = self.lambtha
+            if self.lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+        else:
+            if type(data) != list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:   
+                self.lambtha = sum(self.data) / len(self.data)
+                
